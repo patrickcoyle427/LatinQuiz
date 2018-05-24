c05295208d472935da49df11ee145f1e91b7ee54
Patrick Coyle <36138086+patrickcoyle427@users.noreply.github.com>
2018-05-24 00:56:29 -0400

    Add files via upload

--- LatinQuiz.py
+++ LatinQuiz.py
@@ -17,15 +17,20 @@ import xml.etree.ElementTree as ET
 
 from PyQt5.QtWidgets import (QApplication, QMainWindow, QHBoxLayout,
                              QVBoxLayout, QPushButton, QAction, QDialog,
-                             QLabel, QStatusBar, QWidget, QGroupBox)
+                             QLabel, QStatusBar, QWidget, QGroupBox,
+                             QLineEdit, QRadioButton, QToolTip,
+                             QButtonGroup)
 
 from PyQt5.QtCore import Qt
 
 # TODO:
 
 # Finish the layout of the game UI
+# Continue working on start new quiz dialog
 
 # Add a way to restart the game with your current settings
+
+# Record questions the user got wrong to help them study?
 
 # In the help or file menu, include way to see the source of the doc
 
@@ -39,17 +44,94 @@ class LatinQuiz(QWidget):
 
     def initUI(self):
 
-        # Window Widget Layout
+        ### Window Widget Layout ###
 
         window_layout = QVBoxLayout()
 
         self.setLayout(window_layout)
 
-        self.word = QLabel('Test', self)
+        self.word = QLabel('', self)
+        # self.word holds current question. It is set by ___ below
         self.word.setAlignment(Qt.AlignCenter)
         self.word.setStyleSheet('font-family: "Arial Black";')
                       
         window_layout.addWidget(self.word)
+
+        self.answer_group = QGroupBox()
+        # Container for the answers
+
+        ### Buttons to Answer Questions ###
+
+        # Each button is enabled when the quiz is started. Their labels will
+        # have the possible answers the to displayed question.
+
+        self.a_button = QPushButton('A', self)
+        self.a_button.setMaximumWidth(50)
+        self.a_button.setEnabled(False)
+        self.a_button.clicked.connect(self.question_answered)
+        
+        self.b_button = QPushButton('B', self)
+        self.b_button.setMaximumWidth(50)
+        self.b_button.setEnabled(False)
+        self.b_button.clicked.connect(self.question_answered)
+        
+        self.c_button = QPushButton('C', self)
+        self.c_button.setMaximumWidth(50)
+        self.c_button.setEnabled(False)
+        self.c_button.clicked.connect(self.question_answered)
+        
+        self.d_button = QPushButton('D', self)
+        self.d_button.setMaximumWidth(50)
+        self.d_button.setEnabled(False)
+        self.d_button.clicked.connect(self.question_answered)
+
+        self.a_label = QLabel('', self)
+        self.a_label.setAlignment(Qt.AlignCenter)
+        
+        self.b_label = QLabel('', self)
+        self.b_label.setAlignment(Qt.AlignCenter)
+        
+        self.c_label = QLabel('', self)
+        self.c_label.setAlignment(Qt.AlignCenter)
+        
+        self.d_label = QLabel('', self)
+        self.d_label.setAlignment(Qt.AlignCenter)
+
+        a_layout = QHBoxLayout()
+        a_layout.addWidget(self.a_button)
+        a_layout.addWidget(self.a_label)
+
+        b_layout = QHBoxLayout()
+        b_layout.addWidget(self.b_button)
+        b_layout.addWidget(self.b_label)
+
+        c_layout = QHBoxLayout()
+        c_layout.addWidget(self.c_button)
+        c_layout.addWidget(self.c_label)
+
+        d_layout = QHBoxLayout()
+        d_layout.addWidget(self.d_button)
+        d_layout.addWidget(self.d_label)
+
+        answer_group_layout = QVBoxLayout()
+        answer_group_layout.addLayout(a_layout)
+        answer_group_layout.addLayout(b_layout)
+        answer_group_layout.addLayout(c_layout)
+        answer_group_layout.addLayout(d_layout)
+
+        self.answer_group.setLayout(answer_group_layout)
+
+        window_layout.addWidget(self.answer_group)
+
+    def question_answered(self):
+
+        # When a button is pressed, the question's answer is checked against
+        # what the user selected. The incorrect buttons become red and the
+        # correct answer turns green. Something alerts the user if they were
+        # correct or not
+
+        pass
+
         
 class LatinMainWindow(QMainWindow):
 
@@ -67,15 +149,15 @@ class LatinMainWindow(QMainWindow):
 
         file_menu = menubar.addMenu('&File')
 
-        new_game = QAction('&New Game', self)
-        new_game.setShortcut('Ctrl+N')
-        new_game.triggered.connect(self.start_new_game)
+        new_quiz = QAction('&New Quiz', self)
+        new_quiz.setShortcut('Ctrl+N')
+        new_quiz.triggered.connect(self.start_new_quiz)
         
         exit_game = QAction('&Exit', self)
         exit_game.setShortcut('Ctrl+Q')
         exit_game.triggered.connect(self.close)
 
-        file_menu.addAction(new_game)
+        file_menu.addAction(new_quiz)
         file_menu.addAction(exit_game)
         
         help_menu = menubar.addMenu('&Help')
@@ -92,13 +174,20 @@ class LatinMainWindow(QMainWindow):
 
         ### Status Bar Settings ###
 
-        self.status_bar_message = QLabel('Click File, then New Game to start!')
+        self.status_bar_message = QLabel('Click File, then New Quiz to start!')
 
         self.statusBar().addWidget(self.status_bar_message)
          
         ### Misc Window Settings ###
 
-        self.setCentralWidget(LatinQuiz())
+        Quiz = LatinQuiz()
+        # Creates an instance of LatinQuiz()
+
+        self.setCentralWidget(Quiz)
+        # Makes LatinQuiz() the widget that the main window uses
+
+        self.previous_settings = []
+        # Container for the last choosen settings, used for restarting a quiz
 
         self.setGeometry(300, 300, 300, 400)
 
@@ -106,24 +195,75 @@ class LatinMainWindow(QMainWindow):
 
         self.show()
 
-    def start_new_game(self):
+    def start_new_quiz(self):
+
+        options = self.start_quiz_options()
 
         # Opens a window to adjust game settings
 
-        pass
+    def start_quiz_options(self):
+
+        # Builds the window that displays when a new quiz begins
+
+        start_quiz_window = QDialog(None, Qt.WindowCloseButtonHint)
+        # Qt.WindowCloseButtonHint Prevents the ? button from appearing
+        # in the dialog window
+
+        start_quiz_layout = QVBoxLayout()
+
+        set_difficulty_container = QGroupBox()
+
+        set_difficulty_layout = QHBoxLayout()
+
+        set_difficulty_group = QButtonGroup()
+        # Groups the difficulty Radio Buttons together
+
+        self.difficulty_buttons = (QRadioButton('Easy'), QRadioButton('Normal'), QRadioButton('Hard'),
+                              QRadioButton('Very Hard'), QRadioButton('Everything'))
+
+        # Difficulties correspond to how common the words in the questions occur in Latin
+        # Easy: 50 Most Common Words
+        # Normal: 100 Most Common Words
+        # Hard: 250 Most Common Words
+        # Very Hard: 500 Most Common Words
+        # Everything: All 1000(ish) Words
+
+        id_num = 0
+
+        for button in self.difficulty_buttons:
+
+            set_difficulty_group.addButton(button)
+            set_difficulty_group.setId(button, id_num)
+
+            id_num += 1
+
+            set_difficulty_layout.addWidget(button)
+
+        set_difficulty_container.setLayout(set_difficulty_layout)
+
+
+        start_quiz_layout.addWidget(set_difficulty_container)
+
+        start_quiz_window.setGeometry(300, 300, 200, 300)
+
+        start_quiz_window.setWindowTitle('Start New Quiz')
+
+        start_quiz_window.setLayout(start_quiz_layout)
+
+        start_quiz_window.exec_()
 
     def about(self):
         
         # Credits Dickinson College for their amazing
         # xml document that this is built on.
 
-        # plus A little credit screen for me.
+        # plus a little credit screen for me.
 
         pass
 
     def how_to_play(self):
 
-        # Tells the user how to set the game up, the rules,
+        # Tells the user how to set the quiz up, the rules,
         # and what exactly this will do.
 
         pass
